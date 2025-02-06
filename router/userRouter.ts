7492b442821bb6d1b6548f34a1cfbb05c21d3340
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-02-06 20:21:57 +0800

    schema built

--- router/userRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { addUser } from "../controllers/add-user";
-
-export const userRouter = Router();
-
-userRouter.post("/", addUser);
---
+++ src/controllers/user/add-user.ts
@@ -0,0 +1,8 @@
+import { prisma } from "../..";
+
+// export const addUser = async () => {
+// 	const user = await prisma.user.create({
+// 		data: { email: "tsoomoo@gmail.com", username: "tsoomoo" },
+// 	});
+// 	console.log(user);
+// };

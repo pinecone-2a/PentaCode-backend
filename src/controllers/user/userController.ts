bdf65b0fa02e6b83e56e52764d3ef1a4cf67f240
Khosbayar707 <torutora0415@gmail.com>
2025-02-11 17:59:08 +0800

    otp schema

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -1,84 +1,95 @@
 import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
 import { PrismaClient } from "@prisma/client";
-import { request } from "http";
 
 const prisma = new PrismaClient();
 
 export const users = async (req: Request, res: Response) => {
-	try {
-		const user = await prisma.user.findMany();
-		res.json(user);
-	} catch (e) {
-		console.error(e, "error here --->");
-	}
+  try {
+    const user = await prisma.user.findMany();
+    res.json(user);
+  } catch (e) {
+    console.error(e, "error here --->");
+  }
 };
 
 export const addUser = async (req: Request, res: Response) => {
-	const { email, password, username } = req.body;
+  const { email, password, username } = req.body;
 
-	try {
-		if (password) {
-			const rounds = 10;
-			const encryptedPass = bcrypt.hashSync(password, rounds);
-			const newUser = await prisma.user.create({
-				data: {
-					email,
-					password: encryptedPass,
-					username,
-				},
-			});
-			console.log("checking");
-			res.json({ message: "successfully added", newUser });
-		}
-	} catch (e) {
-		console.error(e, "error to add new user ====>");
-	}
+  try {
+    if (password) {
+      const rounds = 10;
+      const encryptedPass = bcrypt.hashSync(password, rounds);
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          password: encryptedPass,
+          username,
+        },
+      });
+      console.log("checking");
+      res.json({ message: "successfully added", newUser });
+    }
+  } catch (e) {
+    console.error(e, "error to add new user ====>");
+  }
 };
 
 export const checkUser = async (req: any, res: any) => {
-	const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-	try {
-		const existingUser = await prisma.user.findUnique({ where: { username } });
-		if (existingUser) {
-			return res.status(409).json({ message: "Username has already taken" });
-		} else {
-			const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { username } });
+    if (existingUser) {
+      return res.status(409).json({ message: "Username has already taken" });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-			const newUser = await prisma.user.create({
-				data: {
-					email,
-					password: hashedPassword,
-					username,
-				},
-			});
-			console.log("User created:", newUser);
-			return res
-				.status(201)
-				.json({ message: "Successfully added", id: newUser.id });
-		}
-	} catch (error) {
-		console.error("Error creating user:", error);
-		return res.status(500).json({ message: "Internal server error" });
-	}
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+          username,
+        },
+      });
+      console.log("User created:", newUser);
+      return res
+        .status(201)
+        .json({ message: "Successfully added", id: newUser.id });
+    }
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const verifyUser = async (req: any, res: any) => {
-	const { email, password } = req.body;
-	try {
-		const user = await prisma.user.findUnique({ where: { email } });
-		if (!user) {
-			return res.status(404).json({ message: "Email not found" });
-		}
-		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  const { email, password } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: "Email not found" });
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
-		if (!isPasswordCorrect) {
-			return res.status(401).json({ message: "Incorrect password" });
-		}
-		res.json({ message: "Login successful", id: user.id });
-	} catch (error) {
-		console.error("Error verifying user:", error);
-		res.status(500).json({ message: "Internal server error" });
-	}
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+    res.json({ message: "Login successful", id: user.id });
+  } catch (error) {
+    console.error("Error verifying user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const forgotPassword = async (req: Request, res: Response) => {
+  const { email } = req.body;
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (user) {
+      const otp = Math.floor(Math.random() * 899999 + 100000);
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Error to catch email" });
+  }
 };

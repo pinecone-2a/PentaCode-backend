ca0e07dc639dac0606cd7788eac93fd3e6f5f1a6
24LP7757 24LP7757 <24LP7757@G302-CC0093.local>
2025-02-14 20:07:09 +0800

    commit

--- src/router/profileRouter.ts
+++ src/router/profileRouter.ts
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import {
-	createProfile,
-	currentUser,
-	editProfile,
-	getExplore,
-	viewProfile,
+  createProfile,
+  currentUser,
+  editProfile,
+  getExplore,
+  viewProfile,
+  viewProfileForHome,
 } from "../controllers/profile/profileController";
 
 export const profileRouter = Router();
 
 profileRouter.get("/view/:profileId", viewProfile);
+profileRouter.get("/viewHome/:userId", viewProfileForHome);
 profileRouter.get("/currentuser/:userId", currentUser);
 profileRouter.get("/explore", getExplore);
 profileRouter.post("/:userId", createProfile);

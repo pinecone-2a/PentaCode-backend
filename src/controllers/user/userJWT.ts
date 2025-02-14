83cd92725a091bb6bc76783b2271d1cc906ca033
Khosbayar707 <torutora0415@gmail.com>
2025-02-14 15:12:28 +0800

    2

--- src/controllers/user/userJWT.ts
+++ src/controllers/user/userJWT.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId: string) => {
   const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: "25h",
+    expiresIn: "1y",
   });
   return token;
 };

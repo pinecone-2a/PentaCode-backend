32e564cf8bdb00d74eaad22c9d7f36a65ffe5992
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-02-17 18:19:15 +0800

    changed expiration

--- src/controllers/user/userJWT.ts
+++ src/controllers/user/userJWT.ts
@@ -4,8 +4,8 @@ import { PrismaClient } from "@prisma/client";
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId: string) => {
-  const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
-    expiresIn: "5s",
-  });
-  return token;
+	const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
+		expiresIn: "15m",
+	});
+	return token;
 };

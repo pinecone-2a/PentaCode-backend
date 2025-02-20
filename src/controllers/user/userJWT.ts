a59efcdbbd72d7c5f580ffe712566f9132ed01cd
24LP0846 24LP0846 <24LP0846@G302-CC0089.local>
2025-02-20 14:41:07 +0800

    jwt prolonged

--- src/controllers/user/userJWT.ts
+++ src/controllers/user/userJWT.ts
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (userId: string) => {
 	const token = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET!, {
-		expiresIn: "15m",
+		expiresIn: "2h",
 	});
 	return token;
 };
